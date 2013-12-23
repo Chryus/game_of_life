764ff9412c4ee3d7ab8fd06641643c12d754bc1e
Chris <chris.haack@me.com>
2013-12-23 09:47:02 -0500

    write test to see if game seeds properly

--- lib/game.rb
+++ lib/game.rb
@@ -7,8 +7,8 @@ class Game
 	attr_accessor :world, :seeds
 
 	def initialize(world=World.new, seeds=[])
-		# @world = world
-		# @seeds = seeds
+		@world = world
+		@seeds = seeds
 	end
 	
 end
--- spec_game_of_life.rb
+++ spec_game_of_life.rb
@@ -6,72 +6,85 @@ Dir.glob('./lib/*.rb') do |model|
   require_relative model
 end
 
-describe 'World' do
-  subject { World.new }
-  
-  it 'should create a new world subject' do
-    subject.is_a?(World).should be_true
-  end
+describe 'Game of life' do
 
-  it 'should respond to proper methods' do 
-    subject.should respond_to(:rows)
-    subject.should respond_to(:cols)
-    subject.should respond_to(:cell_grid)
-  end
+  let!(:world) { World.new }
 
-  it 'should create proper cell grid upon initialization' do
-    subject.cell_grid.is_a?(Array).should be_true
-    subject.cell_grid.each do |row| 
-      row.is_a?(Array).should be_true
-      row.each do |col| 
-        col.is_a?(Cell).should be_true
+  context 'World' do
+    subject { World.new }
+    
+    it 'should create a new world subject' do
+      subject.is_a?(World).should be_true
+    end
+
+    it 'should respond to proper methods' do 
+      subject.should respond_to(:rows)
+      subject.should respond_to(:cols)
+      subject.should respond_to(:cell_grid)
+    end
+
+    it 'should create proper cell grid upon initialization' do
+      subject.cell_grid.is_a?(Array).should be_true
+      subject.cell_grid.each do |row| 
+        row.is_a?(Array).should be_true
+        row.each do |col| 
+          col.is_a?(Cell).should be_true
+        end
       end
     end
+
+  end
+
+  context 'Cell' do
+    subject { Cell.new }
+
+    it 'should create a new cell subject' do
+      subject.is_a?(Cell).should be_true
+    end
+
+    it 'should respond to proper methods' do
+      subject.should respond_to(:alive)
+      subject.should respond_to(:x)
+      subject.should respond_to(:y)
+    end
+
+    it 'should initialize properly' do
+      subject.alive.should be_false
+      subject.x.should eq(0)
+      subject.y.should eq(0)
+    end
+
+  end
+
+
+  context 'Game' do
+
+    subject { Game.new }
+
+    it 'should create a new game subject' do
+      subject.is_a?(Game).should be_true
+    end
+
+    it 'should respond to proper methods' do
+      subject.should respond_to(:world)
+      subject.should respond_to(:seeds)
+    end
+
+    it 'should initialize properly' do
+      subject.world.is_a?(World).should be_true
+      subject.seeds.is_a?(Array).should be_true
+    end
+
+    it 'should seed properly' do
+      game = Game.new(world, [[1, 2], [0,2]])
+    end
+  
   end
 
 end
 
-describe 'Cell' do
-  subject { Cell.new }
-
-  it 'should create a new cell subject' do
-    subject.is_a?(Cell).should be_true
-  end
-
-  it 'should respond to proper methods' do
-    subject.should respond_to(:alive)
-    subject.should respond_to(:x)
-    subject.should respond_to(:y)
-  end
-
-  it 'should initialize properly' do
-    subject.alive.should be_false
-    subject.x.should eq(0)
-    subject.y.should eq(0)
-  end
-
-end
 
 
-describe 'Game' do
-
-  subject { Game.new }
-
-  it 'should create a new game subject' do
-    subject.is_a?(Game).should be_true
-  end
-
-  it 'should respond to proper methods' do
-    subject.should respond_to(:world)
-    subject.should respond_to(:seeds)
-  end
-
-  it 'should initialize properly' do
-    subject.world.is_a?(World).should be_true
-    subject.seeds.is_a?(Array).should be_true
-  end
-
-end
 
 # context 'Rules' do
 
