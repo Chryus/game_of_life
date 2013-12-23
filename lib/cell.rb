d37ca561b009dcb721cce2a4273c427c2c1d8bed
Chris <chris.haack@me.com>
2013-12-23 07:39:36 -0500

    create game class

--- lib/cell.rb
+++ lib/cell.rb
@@ -3,12 +3,20 @@ require_relative './world.rb'
 
 class Cell
 
-	attr_accessor :alive, :x, :y
+  attr_accessor :alive, :x, :y
 
-	def initialize(x=0, y=0)
-		@alive = false
-		@x = x
-		@y = y
-	end
+  def initialize(x=0, y=0)
+    @alive = false
+    @x = x
+    @y = y
+  end
+
+  def find_coordinate(coordinate)
+    results = World.cell_grid.each do |row|
+      row.each do |cell|
+        cell.coordinate
+      end
+    end
+  end
 
 end
