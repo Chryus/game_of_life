8deb3939145fdc8a92ca75f414bc48ed9b46a670
Chris <chris.haack@me.com>
2013-12-27 20:15:05 -0500

    add color to background

--- gosu_game_of_life.rb
+++ gosu_game_of_life.rb
@@ -1,4 +1,29 @@
+#gosu file
+
 require 'gosu'
 Dir.glob('/*.rb') do |model|
   require_relative model
 end
+
+class GameOfLifeWindow < Gosu::Window
+
+	def initialize
+		super 800, 600, false
+		self.caption = "Gosu Tutorial Game"
+
+		#color
+		@background_color = Gosu::Color.new(0xffdedede)
+	end
+
+	def update
+	end
+
+	def draw
+		draw_quad(0, 0, @background_color, line)
+	end
+
+	def needs_cursor?; true; end
+
+end
+
+GameOfLifeWindow.new.show
