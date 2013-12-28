7ae3e2928fd2452f8eb03b73f9a4363bfa8b6b2d
Chris <chris.haack@me.com>
2013-12-28 14:30:12 -0500

    set cols and rows vars and initialize game and world in gosu

--- gosu_game_of_life.rb
+++ gosu_game_of_life.rb
@@ -15,16 +15,23 @@ class GameOfLifeWindow < Gosu::Window
 
 		#color
 		@background_color = Gosu::Color.new(0xffdedede)
+
+		#game itself
+		@cols = width/10
+		@rows = height/10
+		@world = World.new(@cols, @rows)
+		@game = Game.new(@world)
+		#@game.world.randomly_populate
 	end
 
 	def update
 	end
 
 	def draw
-		draw_quad(0, 0, @background_color,
-							width, 0, @background_color, #top right corner of screen
-							width, height, @background_color, #bottom right corner of screen
-							0, height, @background_color) #bottom left corner
+		draw_quad(0, 0, @background_color, #top left corner of scree
+							width, 0, @background_color, #top right corner 
+							width, height, @background_color, #bottom right
+							0, height, @background_color) #bottom left
 	end
 
 	def needs_cursor?; true; end
