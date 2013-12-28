ba66b352436906266acc3e0df8f7394103beec20
Chris <chris.haack@me.com>
2013-12-27 20:23:11 -0500

    add height and width to initiatlize method

--- gosu_game_of_life.rb
+++ gosu_game_of_life.rb
@@ -7,8 +7,10 @@ end
 
 class GameOfLifeWindow < Gosu::Window
 
-	def initialize
-		super 800, 600, false
+	def initialize(height=800, width=600)
+		@height = height
+		@width = width
+		super height, width, false
 		self.caption = "Gosu Tutorial Game"
 
 		#color
@@ -19,7 +21,10 @@ class GameOfLifeWindow < Gosu::Window
 	end
 
 	def draw
-		draw_quad(0, 0, @background_color, line)
+		draw_quad(0, 0, @background_color,
+							width, 0, @background_color, #top right corner of screen
+							width, height, @background_color, #bottom right corner of screen
+							0, height, @background_color) #bottom left corner
 	end
 
 	def needs_cursor?; true; end
